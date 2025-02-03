a5c7c0b21a0351c5c13db40686b11c946dc353fe
Talentjoe <88471172+Talentjoe@users.noreply.github.com>
2025-02-02 18:14:45 -0800

    Initial commit

---
+++ OllamaChatCore.cs
@@ -0,0 +1,169 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace VpetChatWithOllama
+{
+    class OllamaChatCore
+    {
+
+        private String moduleName;
+        private String terminal;
+        private bool supportTool;
+        private bool AddTimeToPrompt;
+        private HttpClient sharedClient;
+        private List<Dictionary<String, String>> chatingHistory;
+        private String prompt;
+        public long tockenCount { get; private set; }
+        public long promptCount { get; private set; }
+
+        public OllamaChatCore(
+            String prompt = "",
+            String moduleName = "Qwen2.5:7b",
+            String terminal = "http://localhost:11434/",
+            bool supportTool = true,
+            bool AddTime = true
+        )
+        {
+            this.moduleName = moduleName.ToLower();
+            this.terminal = terminal;
+            this.supportTool = supportTool;
+            this.chatingHistory = new List<Dictionary<String, String>>();
+            this.prompt = prompt;
+            this.AddTimeToPrompt = AddTime;
+
+            if (prompt != "")
+            {
+                chatingHistory.Add(new Dictionary<String, String>() { { "role", "system" }, { "content", prompt } });
+            }
+
+            this.tockenCount = 0;
+            this.promptCount = 0;
+
+            sharedClient = new()
+            {
+                BaseAddress = new Uri(terminal),
+            };
+
+            //if (!CheckModuleExist().Result)
+            //{
+            //    throw new Exception("Module not found");
+            //}
+        }
+        struct ChatResponse
+        {
+            public Dictionary<String, String> message { get; set; }
+            public int prompt_eval_count { get; set; }
+            public int eval_count { get; set; }
+        }
+
+        struct ModuleExistResponse
+        {
+            public struct Module
+            {
+                public String name { get; set; }
+                public Dictionary<String, String> details { get; set; }
+            }
+            public List<Module> models { get; set; }
+        }
+
+        private async Task<bool> CheckModuleExist()
+        {
+            try
+            {
+                JObject modules = await GetResponse(null, "api/tags");
+
+                foreach (var module in modules["models"])
+                {
+                    String currentName = ((String)module["name"]).ToLower();
+                    if (currentName == moduleName)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Failed to get modules, Check if ollama is running.");
+            }
+        }
+
+        private Dictionary<String, String> SystemPrompt()
+        {
+            StringBuilder systemPrompt = new StringBuilder();
+            if (AddTimeToPrompt)
+            {
+                systemPrompt.AppendLine("current time: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            }
+
+            return new Dictionary<String, String>() { { "role", "system" }, { "content", systemPrompt.ToString() } };
+        }
+
+        public async Task<String> chat(String nextSentence)
+        {
+
+            chatingHistory.Add(new Dictionary<String, String>() { { "role", "user" }, { "content", nextSentence } });
+
+            chatingHistory.Add(SystemPrompt());
+
+            using StringContent jsonContent = new(
+                System.Text.Json.JsonSerializer.Serialize(new
+                {
+                    model = moduleName,
+                    messages = chatingHistory,
+                    stream = false
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            //ChatResponse chatResponse = await GetResponse<ChatResponse>(jsonContent, "api/chat");
+            JObject chatResponseJson = await GetResponse(jsonContent, "api/chat");
+
+            ChatResponse chatResponse = chatResponseJson.ToObject<ChatResponse>();
+
+            promptCount += chatResponse.prompt_eval_count;
+            tockenCount += chatResponse.eval_count;
+
+            chatingHistory.Add(new Dictionary<string, string>(chatResponse.message));
+
+            return chatResponse.message["content"];
+        }
+
+
+        private async Task<JObject> GetResponse(StringContent sendingMessage, String url)
+        {
+            HttpResponseMessage response;
+
+            if (sendingMessage == null)
+            {
+                response = await sharedClient.GetAsync(url);
+            }
+            else
+            {
+                response = await sharedClient.PostAsync(url, sendingMessage);
+            }
+
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(jsonResponse);
+            }
+
+            if (jsonResponse == null)
+            {
+                return default;
+            }
+
+            JObject jobject = JObject.Parse(jsonResponse);
+            //T formatedResponse = JsonConvert.DeserializeObject<T>(jsonResponse);
+
+            return jobject;
+        }
+    }
+}
---
+++ VpetChatWithOllama.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VPet_Simulator.Windows.Interface;
+
+namespace VpetChatWithOllama
+{
+    public class VpetChatWithOllama : MainPlugin
+    {
+        public VpetChatWithOllama(IMainWindow mainwin) : base(mainwin) { }
+        public override void LoadPlugin()
+        {
+            MW.TalkAPI.Add(new ChatOllamaAPI(this));
+        }
+
+        public override void Save()
+        {
+        }
+        public override void Setting()
+        {
+        }
+        public override string PluginName => "ChatGPT";
+    }
+
+    public class ChatOllamaAPI: TalkBox
+    {
+        OllamaChatCore OllamaChatCorer;
+        public ChatOllamaAPI(VpetChatWithOllama mainPlugin) : base(mainPlugin)
+        {
+            try
+            {
+                OllamaChatCorer = new OllamaChatCore(prompt: "你是一只猫娘,你叫爱丽丝");
+
+            }
+            catch (Exception e)
+            {
+                DisplayThinkToSayRnd("OllamaChatCore初始化失败");
+            }
+            Plugin = mainPlugin;
+        }
+        protected VpetChatWithOllama Plugin;
+        public override string APIName => "ChatOllama";
+        public override async void Responded(string text)
+        {
+            DisplayThink();
+            String res =  await OllamaChatCorer.chat(text);
+
+            DisplayThinkToSayRnd(res);
+        }
+        public override void Setting()
+        {
+         
+        }
+    }
+}
